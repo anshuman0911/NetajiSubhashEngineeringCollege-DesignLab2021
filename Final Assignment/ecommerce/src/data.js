bd8fbf31289bc4697a16fa099a21880d6e1a5852
anshuman0911 <61405900+anshuman0911@users.noreply.github.com>
2021-07-04 19:28:05 +0530

    Add files via upload

---
+++ Final Assignment/ecommerce/src/data.js
@@ -0,0 +1,29 @@
+const data = {
+    products: [
+      {
+        id: '1',
+        name: 'Oneplus 9',
+        price: 39999,
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRguUQsyr55xMpdI3X3xPRpZb--ld9eoUd97g&usqp=CAU',
+      },
+      {
+        id: '2',
+        name: 'iphone 12',
+        price: 84000,
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpU_YrEWb00hgbzZQEXvUzmvYIzXnR6zBYag&usqp=CAU',
+      },
+      {
+        id: '3',
+        name: 'Poco x3',
+        price: 14999,
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPp9yGdIIoLrkAKIgt58ZcunZ2KF24nqMvcA&usqp=CAU',
+      },
+      {
+        id: '4',
+        name: 'Realme 7',
+        price: 13450,
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYl62Q17XzNwkZyf6CPXmg-4baAXFYenjlJQ&usqp=CAU',
+      },
+    ],
+  };
+  export default data;
